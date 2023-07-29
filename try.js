007687ccdb5274dd181c7ee68f77a99eb40efcff
Selishaa <selishathapa5@gmail.com>
2023-07-28 23:16:41 -0400

    finally done

---
+++ try.js
@@ -0,0 +1,15 @@
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $(".fadein")
+
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i]
+
+      if ($(tag).offset().top < pageBottom) {
+        $(tag).addClass("visible")
+      } else {
+        $(tag).removeClass("visible")
+      }
+    }
+  })
